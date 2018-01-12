783336385d4e3f24c107f89cfe44d0df23e11aa6
S191617 <34568625+S191617@users.noreply.github.com>
2018-01-12 10:24:48 +0000

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,12 @@
+portion_deposit = 0.2
+current_savings = 0
+monthly_salary= annual_salary/12
+monthly_interest = current_savings *(0.04 / 12)
+
+total_cost = float(input("Total cost of the house"))
+annual_salary= float(input("How much do you earn?"))
+portion_saved= float(input("How much money do you want to save?"))
+
+months _required = ((portion_deposit * total_cost) / ((monthly_salary * portion_saved) + monthly_interest))
+
+print("Number of months:" , months_required)
